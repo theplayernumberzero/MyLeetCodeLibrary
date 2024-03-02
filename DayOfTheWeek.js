e31399946938c855ddf91f0fbc57e96d17d332d2
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-03-02 10:53:34 +0300

    day 11

---
+++ DayOfTheWeek.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+    const date = new Date(`${year}-${month}-${day}`);
+    let today = date.getDay();
+    switch(today){
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";            
+    }
+};
