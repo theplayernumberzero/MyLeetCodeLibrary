4c35cd8772774e71345355fa4a69a97c764ac5ca
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-03-26 10:42:47 +0300

    day 25

---
+++ NumberOfStepsToReduceANumberToZero.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count=0;
+    while(num>0){
+        if(num%2==0){
+            num/=2;
+            count++;
+        }
+        else{
+            num-=1;
+            count++;
+        }
+    }
+    return count;
+};
