5907a033769b299bb522a0ae96c30f81e470553b
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-04-03 12:05:00 +0300

    day 31

---
+++ DetectCapital.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    if(word.toLowerCase() == word || word.toUpperCase() == word){
+        return true;
+    }
+    else if((word[0] + word.slice(1).toLowerCase()) == word){
+        return true; 
+    }
+    else{ 
+        return false;
+    }
+};
